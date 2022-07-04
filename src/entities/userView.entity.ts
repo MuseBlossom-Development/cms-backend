a5c36f4cd520eedd376ec5ba8c9d44d71c4985d4
shan <97424268+jeongmisnu@users.noreply.github.com>
2022-07-04 13:53:41 +0900

    Fead: "추가 MySQL UsersView"
    유저 정보를 가져올 때마다 조인을 하는 것 보다 뷰를 만드는 것이 효율적이라고 생각되어 추가

--- src/entities/userView.entity.ts
+++ src/entities/userView.entity.ts
@@ -6,17 +6,27 @@ import { UserInfo } from './userinfo.entity';
   expression: (dataSource: DataSource) =>
     dataSource
       .createQueryBuilder()
-      .select('', 'id')
-      .addSelect('', '')
-      .addSelect('', '')
-      .addSelect('', '')
-      .addSelect('', '')
-      .from(Users, 'u')
-      .leftJoin(UserInfo, 'info', 'user.user_id = info.user'),
+      .select('u.user_id', 'id')
+      .addSelect('info.business_number', 'business_number')
+      .addSelect('u.company_name', 'company_name')
+      .addSelect('u.tier', 'tier')
+      .addSelect('info.manager', 'manager')
+      .addSelect('u.email', 'email')
+      .addSelect('info.contact_info', 'contact_info')
+      .addSelect('u.subscription', 'subscription')
+      .addSelect('u.create_at', 'create_at')
+      .from(UserInfo, 'info')
+      .leftJoin(Users, 'u', 'u.user_id = info.user_id'),
 })
 export class UserView {
   @ViewColumn()
   id: string;
+
+  @ViewColumn()
+  business_number: string;
+
+  @ViewColumn()
+  company_name: string;
 
   @ViewColumn()
   tier: number;
@@ -34,11 +44,5 @@ export class UserView {
   subscription: Date;
 
   @ViewColumn()
-  company_name: string;
-
-  @ViewColumn()
-  business_number: string;
-
-  @ViewColumn()
-  business_type: string;
+  create_at: Date;
 }
