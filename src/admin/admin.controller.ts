418687a8ba1e9867b6dd8d4778b846a44d404ac6
shan <97424268+jeongmisnu@users.noreply.github.com>
2022-07-08 17:53:25 +0900

    Fead: "추가 관리자 관련 기능추가"

---
+++ src/admin/admin.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { AdminService } from './admin.service';
+
+@Controller('admin')
+export class AdminController {
+  constructor(private readonly adminService: AdminService) {}
+  // 유저 목록
+  @Get('list')
+  async getUsers(@Query('grade') tier: string) {
+    return await this.adminService.getUsers(+tier);
+  }
+}
