eb157c4b2c973d13af8979d8d7d9b852bb57bb71
shan <97424268+jeongmisnu@users.noreply.github.com>
2022-08-08 15:09:44 +0900

    Fix: "API 서버 IP로 접속시에 메인 주소로 리디렉션됨"

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,11 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Redirect(
+    'http://museblosson-asp-test.s3-website.ap-northeast-2.amazonaws.com/',
+    301,
+  )
   getHello(): string {
-    return this.appService.getHello();
+    return;
   }
 }
